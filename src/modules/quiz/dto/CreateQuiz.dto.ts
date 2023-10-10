61422f5ff49fd3d73fb8a2267d4d26a2fd71d79e
Okunade Adeola <adeolafunke7@gmail.com>
2023-10-10 14:00:23 +0100

    updated

--- src/modules/quiz/dto/CreateQuiz.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty } from 'class-validator';
-
-export class createQuizDto {
-  @IsNotEmpty({ message: 'title must not be empty' })
-  title: string;
-
-  @IsNotEmpty({ message: 'description must not be empty' })
-  description: string;
-}
